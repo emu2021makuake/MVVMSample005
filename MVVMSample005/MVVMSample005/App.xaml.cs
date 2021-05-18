7ae331d798a8a706b5cd84f5d42b13ddfa24dfb1
emu2021makuake <81235941+emu2021makuake@users.noreply.github.com>
2021-05-18 18:50:56 +0900

    Delete MVVMSample005 directory

--- MVVMSample005/MVVMSample005/App.xaml.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Windows;
-
-namespace MVVMSample005
-{
-    public partial class App : Application
-    {
-    }
-}
--- MVVMSample005/MVVMSample005/Models/Message.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace MVVMSample005.Models
-{
-    class Message
-    {
-        public int Num { get; set; }
-        public string Str { get; set; }
-    }
-}
